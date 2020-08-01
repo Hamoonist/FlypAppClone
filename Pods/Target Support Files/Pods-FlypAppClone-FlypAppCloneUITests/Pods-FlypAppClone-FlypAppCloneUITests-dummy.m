07bab3d61cabe64bd7cd3f28913515cd2f2037ca
hamoon jamshidi meydandar <jamshidimeydandar@gmail.com>
2020-08-01 08:32:09 +0200

    initial commit
    Set up with Cocopods

---
+++ Pods/Target Support Files/Pods-FlypAppClone-FlypAppCloneUITests/Pods-FlypAppClone-FlypAppCloneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlypAppClone_FlypAppCloneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FlypAppClone_FlypAppCloneUITests
+@end
---
+++ Pods/Target Support Files/Pods-FlypAppCloneTests/Pods-FlypAppCloneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlypAppCloneTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlypAppCloneTests
+@end
